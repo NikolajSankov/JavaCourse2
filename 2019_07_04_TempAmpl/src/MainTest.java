527106c7b29eb7bc91033c6abeb00e9b1e452f15
NikolajSankovDev <nikolaj.sankov.dev@gmail.com>
2019-07-06 10:19:45 +0200

    new file

---
+++ 2019_07_04_TempAmpl/src/MainTest.java
@@ -0,0 +1,22 @@
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+		
+	}
+	
+}
